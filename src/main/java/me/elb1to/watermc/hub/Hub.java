5f41568e8c4e6beb9fcf4a3be5164fa2b2fbb440
Elb1to <elbitomc@gmail.com>
2021-01-30 22:21:04 -0500

    Update Hub.java

--- src/main/java/me/elb1to/watermc/hub/Hub.java
+++ src/main/java/me/elb1to/watermc/hub/Hub.java
@@ -1,6 +1,9 @@
 package me.elb1to.watermc.hub;
 
 import lombok.Getter;
+import me.elb1to.watermc.hub.database.Database;
+import me.elb1to.watermc.hub.managers.vault.VaultManager;
+import me.elb1to.watermc.hub.utils.command.CommandFramework;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
@@ -8,9 +11,15 @@ public final class Hub extends JavaPlugin {
 
 	@Getter private static Hub instance;
 
+	private Database databaseHandler;
+	private VaultManager vaultManager;
+	private CommandFramework commandFramework;
+
 	@Override
 	public void onEnable() {
 		instance = this;
+
+		commandFramework = new CommandFramework(this);
 	}
 
 	@Override
