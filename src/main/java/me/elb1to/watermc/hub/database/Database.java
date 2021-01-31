9e6d0841d3f15fba19433b825cd18fa1f9a38baf
Elb1to <elbitomc@gmail.com>
2021-01-30 22:20:02 -0500

    Update Database.java

--- src/main/java/me/elb1to/watermc/hub/database/Database.java
+++ src/main/java/me/elb1to/watermc/hub/database/Database.java
@@ -6,10 +6,9 @@ import java.util.UUID;
 
 /**
  * Created by Elb1to
- * Project: FrozedHubDeluxe
+ * Project: Hub [WaterMC]
  * Date: 30/01/2020 @ 19:28
  */
-
 public interface Database {
 	Document loadData(UUID uuid, String name);
 
