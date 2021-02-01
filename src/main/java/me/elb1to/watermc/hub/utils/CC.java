4e0e60181341f72c0a3e25d201e60aed985ff113
Elb1to <elbitomc@gmail.com>
2021-02-01 18:37:06 -0500

    Added SB line and bukkit color getter for armors

--- src/main/java/me/elb1to/watermc/hub/utils/CC.java
+++ src/main/java/me/elb1to/watermc/hub/utils/CC.java
@@ -1,6 +1,7 @@
 package me.elb1to.watermc.hub.utils;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,11 +13,12 @@ import java.util.List;
  */
 public class CC {
 
+	public static String SB_BAR = ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH.toString() + "----------------------";
 	public static String MENU_BAR = ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH.toString() + "------------------------";
 	public static String CHAT_BAR = ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH.toString() + "------------------------------------------------";
 
-	public static String translate(String string) {
-		return ChatColor.translateAlternateColorCodes('&', string);
+	public static String translate(String s) {
+		return ChatColor.translateAlternateColorCodes('&', s);
 	}
 
 	public static List<String> translate(List<String> lines) {
@@ -43,5 +45,45 @@ public class CC {
 		}
 
 		return toReturn;
+	}
+
+	public static Color getBukkitColor(String color) {
+		String name = color.toUpperCase();
+		switch (name) {
+			case "RED":
+				return Color.RED;
+			case "BLUE":
+				return Color.BLUE;
+			case "BLACK":
+				return Color.BLACK;
+			case "YELLOW":
+				return Color.YELLOW;
+			case "AQUA":
+				return Color.AQUA;
+			case "FUCHSIA":
+				return Color.FUCHSIA;
+			case "GRAY":
+				return Color.GRAY;
+			case "GREEN":
+				return Color.GREEN;
+			case "MAROON":
+				return Color.MAROON;
+			case "NAVY":
+				return Color.NAVY;
+			case "ORANGE":
+				return Color.ORANGE;
+			case "LIME":
+				return Color.LIME;
+			case "OLIVE":
+				return Color.OLIVE;
+			case "PURPLE":
+				return Color.PURPLE;
+			case "TEAL":
+				return Color.TEAL;
+			case "SILVER":
+				return Color.SILVER;
+		}
+
+		return Color.WHITE;
 	}
 }
