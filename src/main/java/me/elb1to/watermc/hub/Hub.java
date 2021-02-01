c43f09bda83f897ca71aa15844cfc3c5444a08d7
Elb1to <elbitomc@gmail.com>
2021-02-01 18:58:45 -0500

    Registered listeners, managers and instantiated classes + tasks

--- src/main/java/me/elb1to/watermc/hub/Hub.java
+++ src/main/java/me/elb1to/watermc/hub/Hub.java
@@ -1,29 +1,204 @@
 package me.elb1to.watermc.hub;
 
+import club.frozed.tablist.FrozedTablist;
+import com.google.common.collect.Maps;
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import lombok.Getter;
-import me.elb1to.watermc.hub.database.Database;
-import me.elb1to.watermc.hub.managers.vault.VaultManager;
+import lombok.Setter;
+import me.elb1to.watermc.hub.commands.queue.*;
+import me.elb1to.watermc.hub.listeners.DoubleJumpListener;
+import me.elb1to.watermc.hub.listeners.EnderbuttListener;
+import me.elb1to.watermc.hub.listeners.PlayerListener;
+import me.elb1to.watermc.hub.listeners.ServerListener;
+import me.elb1to.watermc.hub.managers.MongoDbManager;
+import me.elb1to.watermc.hub.managers.QueueManager;
+import me.elb1to.watermc.hub.managers.VaultManager;
+import me.elb1to.watermc.hub.providers.ScoreboardProvider;
+import me.elb1to.watermc.hub.providers.TablistProvider;
+import me.elb1to.watermc.hub.tasks.PlayerDataWorkerRunnable;
+import me.elb1to.watermc.hub.user.HubPlayer;
+import me.elb1to.watermc.hub.utils.CC;
 import me.elb1to.watermc.hub.utils.command.CommandFramework;
+import me.elb1to.watermc.hub.utils.config.FileConfig;
+import me.elb1to.watermc.hub.utils.extra.PlayerUtils;
+import me.elb1to.watermc.hub.utils.menu.ButtonListener;
+import me.elb1to.watermc.hub.utils.scoreboard.BoardManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.*;
 
 @Getter
-public final class Hub extends JavaPlugin {
+public final class Hub extends JavaPlugin implements PluginMessageListener {
 
 	@Getter private static Hub instance;
 
-	private Database databaseHandler;
+	private Map<String, Integer> serverPlayers = Maps.newHashMap();
+	private Set<String> bungeeServers = new HashSet<>();
+	private BukkitTask countPlayerTask;
+
 	private VaultManager vaultManager;
 	private CommandFramework commandFramework;
+
+	private QueueManager queueManager;
+
+	private MongoDbManager mongoDbManager;
+	private FileConfig settingsConfig, databaseConfig, queuesConfig, messagesConfig, scoreboardConfig, tablistConfig, hotbarConfig, armorConfig;
+
+	@Setter private boolean restarting = false;
+	private PlayerDataWorkerRunnable playerDataWorkerRunnable;
 
 	@Override
 	public void onEnable() {
 		instance = this;
 
+		if (!this.getDescription().getAuthors().contains("Elb1to") || !this.getDescription().getName().equals("Hub")) {
+			System.exit(0);
+			Bukkit.shutdown();
+		}
+
 		commandFramework = new CommandFramework(this);
+
+		this.armorConfig = new FileConfig(this, "armor.yml");
+		this.hotbarConfig = new FileConfig(this, "hotbar.yml");
+		this.queuesConfig = new FileConfig(this, "queues.yml");
+		this.tablistConfig = new FileConfig(this, "tablist.yml");
+		this.databaseConfig = new FileConfig(this, "database.yml");
+		this.settingsConfig = new FileConfig(this, "settings.yml");
+		this.messagesConfig = new FileConfig(this, "messages.yml");
+		this.scoreboardConfig = new FileConfig(this, "scoreboard.yml");
+
+		new QueueCommand();
+		new QueueInfoCommand();
+		new JoinQueueCommand();
+		new LeaveQueueCommand();
+		new PauseQueueCommand();
+		new LimitQueueCommand();
+		new QueueCheckCommand();
+
+		this.mongoDbManager = new MongoDbManager();
+		this.mongoDbManager.connect();
+
+		this.queueManager = new QueueManager();
+
+		Bukkit.getConsoleSender().sendMessage(CC.CHAT_BAR);
+		Bukkit.getConsoleSender().sendMessage(CC.translate("&bHub [WaterMC] &8- &fv" + getDescription().getVersion()));
+		Bukkit.getConsoleSender().sendMessage(CC.translate("&7Made on &bFrozed Club Development &7by &bElb1to"));
+		Bukkit.getConsoleSender().sendMessage(CC.CHAT_BAR);
+
+		if (settingsConfig.getConfiguration().getBoolean("SETTINGS.GENERAL.TABLIST-ENABLED")) {
+			new FrozedTablist(this, new TablistProvider(), 0, 20);
+		}
+
+		playerDataWorkerRunnable = new PlayerDataWorkerRunnable();
+		Thread thread = new Thread(playerDataWorkerRunnable);
+		thread.setName("Hub PlayerData Worker");
+		thread.start();
+
+		Bukkit.getServer().getPluginManager().registerEvents(new PlayerUtils(), this);
+		registerStuff();
+		startPlayerCountTask();
 	}
 
 	@Override
 	public void onDisable() {
-		instance = null;
+		for (HubPlayer hubPlayer : HubPlayer.getAllData()) {
+			hubPlayer.saveData();
+		}
+
+		this.mongoDbManager.disconnect();
+		if (countPlayerTask != null) {
+			countPlayerTask.cancel();
+		}
+	}
+
+	private void registerStuff() {
+		registerListeners();
+
+		Bukkit.getPluginManager().registerEvents(new ButtonListener(), this);
+
+		Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+		Bukkit.getMessenger().registerIncomingPluginChannel(this, "BungeeCord", this);
+
+		new BoardManager(new ScoreboardProvider(), 2);
+	}
+
+	private void registerListeners() {
+		Arrays.asList(
+				new ServerListener(),
+				new PlayerListener(),
+				new ButtonListener(),
+				new EnderbuttListener(),
+				new DoubleJumpListener()
+		).forEach(listener -> getServer().getPluginManager().registerEvents(listener, this));
+	}
+
+	@Override
+	public void onPluginMessageReceived(String s, Player player, byte[] bytes) {
+		if (!s.equals("BungeeCord")) return;
+		ByteArrayDataInput bytearray = ByteStreams.newDataInput(bytes);
+		String cmd = bytearray.readUTF();
+		if (cmd.equalsIgnoreCase("GetServers")) {
+			Collections.addAll(bungeeServers, bytearray.readUTF().split(", "));
+			return;
+		}
+		if (!cmd.equalsIgnoreCase("PlayerCount")) {
+			return;
+		}
+		String server = bytearray.readUTF();
+		if (this.serverPlayers.containsKey(server)) {
+			this.serverPlayers.remove(server);
+		}
+		this.serverPlayers.put(server, bytearray.readInt());
+	}
+
+	private void startPlayerCountTask() {
+		countPlayerTask = new BukkitRunnable() {
+			public void run() {
+				if (Bukkit.getOnlinePlayers() == null || Bukkit.getOnlinePlayers().isEmpty()) {
+					return;
+				}
+				if (bungeeServers.isEmpty()) {
+					ByteArrayDataOutput send = ByteStreams.newDataOutput();
+					send.writeUTF("GetServers");
+					Bukkit.getOnlinePlayers().iterator().next().sendPluginMessage(Hub.getPlugin(Hub.class), "BungeeCord", send.toByteArray());
+					return;
+				}
+				for (String s : bungeeServers) {
+					ByteArrayDataOutput send = ByteStreams.newDataOutput();
+					send.writeUTF("PlayerCount");
+					send.writeUTF(s);
+					Bukkit.getOnlinePlayers().iterator().next().sendPluginMessage(Hub.getPlugin(Hub.class), "BungeeCord", send.toByteArray());
+				}
+			}
+		}.runTaskTimer(this, 0L, 20L);
+	}
+
+	public int getNetworkTotalPlayer(String server, boolean global) {
+		if (this.serverPlayers.isEmpty()) {
+			return 0;
+		}
+
+		int i = 0;
+		if (global) {
+			for (Map.Entry<String, Integer> e : serverPlayers.entrySet()) {
+				i += e.getValue();
+			}
+		} else {
+			for (Map.Entry<String, Integer> entry : serverPlayers.entrySet()) {
+				if (entry.getKey().equalsIgnoreCase(server)) {
+					i += entry.getValue();
+				}
+			}
+
+		}
+
+		return i;
 	}
 }
