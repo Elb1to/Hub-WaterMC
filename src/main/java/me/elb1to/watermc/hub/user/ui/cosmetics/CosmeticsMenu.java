e03d0ef3797415427daf4aa6d9c2162c6365ecd2
Elb1to <elbitomc@gmail.com>
2021-02-03 01:04:47 -0500

    Create CosmeticsMenu.java

---
+++ src/main/java/me/elb1to/watermc/hub/user/ui/cosmetics/CosmeticsMenu.java
@@ -0,0 +1,25 @@
+package me.elb1to.watermc.hub.user.ui.cosmetics;
+
+import me.elb1to.watermc.hub.utils.menu.Button;
+import me.elb1to.watermc.hub.utils.menu.Menu;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+
+/**
+ * Created by Elb1to
+ * Project: Hub [WaterMC]
+ * Date: 2/2/2021 @ 1:21 PM
+ */
+public class CosmeticsMenu extends Menu {
+
+	@Override
+	public String getTitle(Player player) {
+		return null;
+	}
+
+	@Override
+	public Map<Integer, Button> getButtons(Player player) {
+		return null;
+	}
+}
