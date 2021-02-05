76d6ec50fe57f8f655b4d3d6271bb04035ba233d
Elb1to <elbitomc@gmail.com>
2021-02-05 11:19:49 -0500

    Create ConnectionAPI.java

---
+++ src/main/java/me/elb1to/watermc/hub/ConnectionAPI.java
@@ -0,0 +1,15 @@
+package me.elb1to.watermc.hub;
+
+/**
+ * Created by Elb1to
+ * Project: Hub [WaterMC]
+ * Date: 2/5/2021 @ 10:22 AM
+ */
+public class ConnectionAPI {
+
+	public static int connected(String server) {
+		int players = 0;
+
+		return players;
+	}
+}
