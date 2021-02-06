329c0c927a620c9e261e0c93201087f5ff317ff5
Elb1to <elbitomc@gmail.com>
2021-02-05 19:44:05 -0500

    Added new button for tags and cleaned code

--- src/main/java/me/elb1to/watermc/hub/user/ui/settings/SettingsMenu.java
+++ src/main/java/me/elb1to/watermc/hub/user/ui/settings/SettingsMenu.java
@@ -16,6 +16,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import static me.elb1to.watermc.hub.utils.menu.Button.playNeutral;
+import static me.elb1to.watermc.hub.utils.menu.Button.playSuccess;
+
 /**
  * Created by Elb1to
  * Project: Hub [WaterMC]
@@ -38,14 +41,26 @@ public class SettingsMenu extends Menu {
 	public Map<Integer, Button> getButtons(Player player) {
 		Map<Integer, Button> buttons = new HashMap<>();
 
-		buttons.put(10, new ParticleHiderButton());
-		buttons.put(11, new PlayerHiderButton());
-		buttons.put(12, new PlayerFlightButton());
-		buttons.put(13, new PlayerSpeedButton());
-
-		buttons.put(16, new OpenCosmeticsButton());
+		buttons.put(11, new ParticleHiderButton());
+		buttons.put(12, new PlayerHiderButton());
+		buttons.put(13, new PlayerFlightButton());
+		buttons.put(14, new OpenTagsMenu());
+		buttons.put(15, new OpenCosmeticsButton());
 
 		return buttons;
+	}
+
+	@Override
+	public int size(Map<Integer, Button> buttons) {
+		return 9 * 3;
+	}
+
+	private void playSound(boolean enabled, Player player) {
+		if (enabled) {
+			playSuccess(player);
+		} else {
+			playNeutral(player);
+		}
 	}
 
 	@AllArgsConstructor
@@ -53,36 +68,22 @@ public class SettingsMenu extends Menu {
 
 		@Override
 		public ItemStack getButtonItem(Player player) {
-			//HubPlayer hubPlayer = HubPlayer.getByUuid(player.getUniqueId());
-
-			return new ItemBuilder(Material.FIREBALL)
+			return new ItemBuilder(Material.EMERALD)
 					.setName("&bMostrar Particulas")
 					.setLore(Arrays.asList(
-							CC.MENU_BAR,
 							"&fActualmente " + (hubPlayer.isHidingParticles() ? "&c&locultas" : "&a&lvisibles"),
 							" ",
 							"&7Si están ocultas no podras",
-							"&7ver las particulas de nadie",
-							CC.MENU_BAR
+							"&7ver las particulas de nadie"
 							)
 					).get();
 		}
 
 		@Override
 		public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-			//HubPlayer hubPlayer = HubPlayer.getByUuid(player.getUniqueId());
-
 			playSound(!hubPlayer.isHidingParticles(), player);
 			hubPlayer.setHidingParticles(!hubPlayer.isHidingParticles());
 			player.sendMessage(CC.translate(hubPlayer.isHidingParticles() ? "&cOcultando todas las particulas en el servidor..." : "&aMostrando todas las particulas en el servidor!"));
-		}
-
-		private void playSound(boolean enabled, Player player) {
-			if (enabled) {
-				playSuccess(player);
-			} else {
-				playNeutral(player);
-			}
 		}
 	}
 
@@ -91,36 +92,22 @@ public class SettingsMenu extends Menu {
 
 		@Override
 		public ItemStack getButtonItem(Player player) {
-			//HubPlayer hubPlayer = HubPlayer.getByUuid(player.getUniqueId());
-
 			return new ItemBuilder(Material.EYE_OF_ENDER)
 					.setName("&bMostrar Jugadores")
 					.setLore(Arrays.asList(
-							CC.MENU_BAR,
 							"&fActualmente " + (hubPlayer.isHidingPlayers() ? "&c&locultos" : "&a&lvisibles"),
 							" ",
 							"&7Si los jugadores están ocultos",
-							"&7todos serán invisibles para ti.",
-							CC.MENU_BAR
+							"&7todos serán invisibles para ti."
 							)
 					).get();
 		}
 
 		@Override
 		public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-			//HubPlayer hubPlayer = HubPlayer.getByUuid(player.getUniqueId());
-
 			playSound(!hubPlayer.isHidingPlayers(), player);
 			hubPlayer.setHidingPlayers(!hubPlayer.isHidingPlayers());
 			player.sendMessage(CC.translate(hubPlayer.isHidingPlayers() ? "&cOcultando todas los jugadores en el servidor..." : "&aMostrando todos los jugadores en el servidor!"));
-		}
-
-		private void playSound(boolean enabled, Player player) {
-			if (enabled) {
-				playSuccess(player);
-			} else {
-				playNeutral(player);
-			}
 		}
 	}
 
@@ -129,75 +116,22 @@ public class SettingsMenu extends Menu {
 
 		@Override
 		public ItemStack getButtonItem(Player player) {
-			//HubPlayer hubPlayer = HubPlayer.getByUuid(player.getUniqueId());
-
 			return new ItemBuilder(Material.FEATHER)
 					.setName("&bActivar Vuelo")
 					.setLore(Arrays.asList(
-							CC.MENU_BAR,
-							"&fActualmente " + (hubPlayer.isFlyModeEnabled() ? "&c&ldesactivado" : "&a&lactivado"),
+							"&fActualmente " + (hubPlayer.isFlyMode() ? "&a&lactivado" : "&c&ldesactivado"),
 							" ",
 							"&7Si esta activado podras",
-							"&7volar por todo el Hub.",
-							CC.MENU_BAR
+							"&7volar por todo el Hub."
 							)
 					).get();
 		}
 
 		@Override
 		public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-			//HubPlayer hubPlayer = HubPlayer.getByUuid(player.getUniqueId());
-
-			playSound(!hubPlayer.isFlyModeEnabled(), player);
-			hubPlayer.setFlyModeEnabled(!hubPlayer.isFlyModeEnabled());
-			player.sendMessage(CC.translate(hubPlayer.isFlyModeEnabled() ? "&cHaz desactivado el modo de vuelo." : "&aHaz activado el modo de vuelo!"));
-		}
-
-		private void playSound(boolean enabled, Player player) {
-			if (enabled) {
-				playSuccess(player);
-			} else {
-				playNeutral(player);
-			}
-		}
-	}
-
-	@AllArgsConstructor
-	private class PlayerSpeedButton extends Button {
-
-		@Override
-		public ItemStack getButtonItem(Player player) {
-			//HubPlayer hubPlayer = HubPlayer.getByUuid(player.getUniqueId());
-
-			return new ItemBuilder(Material.SUGAR)
-					.setName("&bActivar Velocidad")
-					.setLore(Arrays.asList(
-							CC.MENU_BAR,
-							"&fActualmente " + (hubPlayer.isSpeedModeEnabled() ? "&c&ldesactivado" : "&a&lactivado"),
-							" ",
-							"&7Si esta activado podras",
-							"&7correr mucho mas rapido",
-							"&7por todo el Hub.",
-							CC.MENU_BAR
-							)
-					).get();
-		}
-
-		@Override
-		public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-			//HubPlayer hubPlayer = HubPlayer.getByUuid(player.getUniqueId());
-
-			playSound(!hubPlayer.isSpeedModeEnabled(), player);
-			hubPlayer.setSpeedModeEnabled(!hubPlayer.isSpeedModeEnabled());
-			player.sendMessage(CC.translate(hubPlayer.isSpeedModeEnabled() ? "&cHaz desactivado tu aumento de velocidad." : "&aHaz activado la velocidad extra!"));
-		}
-
-		private void playSound(boolean enabled, Player player) {
-			if (enabled) {
-				playSuccess(player);
-			} else {
-				playNeutral(player);
-			}
+			playSound(!hubPlayer.isFlyMode(), player);
+			hubPlayer.setFlyMode(!hubPlayer.isFlyMode());
+			player.sendMessage(CC.translate(hubPlayer.isFlyMode() ? "&aHaz activado el modo de vuelo!" : "&cHaz desactivado el modo de vuelo."));
 		}
 	}
 
@@ -206,19 +140,15 @@ public class SettingsMenu extends Menu {
 
 		@Override
 		public ItemStack getButtonItem(Player player) {
-
 			return new ItemBuilder(Material.ENDER_CHEST)
-					.setName("&bCosmetics")
+					.setName("&bParticulas")
 					.setLore(Arrays.asList(
-							CC.MENU_BAR,
 							"&7Al clickear aquí podras",
 							"&7escoger particulas para ti!",
 							" ",
 							"&7Tipos de particulas:",
 							"&8 > &aJugador",
-							"&8 > &cDoble Salto",
-							"&8 > &cAl Shiftear",
-							CC.MENU_BAR
+							"&8 > &aDoble Salto"
 							)
 					).get();
 		}
@@ -229,8 +159,23 @@ public class SettingsMenu extends Menu {
 		}
 	}
 
-    @Override
-    public int size(Map<Integer, Button> buttons) {
-        return 9 * 3;
-    }
+	@AllArgsConstructor
+	private class OpenTagsMenu extends Button {
+
+		@Override
+		public ItemStack getButtonItem(Player player) {
+			return new ItemBuilder(Material.PAPER)
+					.setName("&bPrefijos")
+					.setLore(Arrays.asList(
+							"&7Al clickear aquí podras",
+							"&7escoger un prefijo para ti!"
+							)
+					).get();
+		}
+
+		@Override
+		public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
+			player.performCommand("tags");
+		}
+	}
 }
