c64d147f5a684ebfc10048d10fa119eb7a60ef26
Victor Abrego <elbitomc@gmail.com>
2021-02-08 19:29:10 -0500

    Updated hub player data worker

--- src/main/java/me/elb1to/watermc/hub/tasks/PlayerDataWorkerRunnable.java
+++ src/main/java/me/elb1to/watermc/hub/tasks/PlayerDataWorkerRunnable.java
@@ -1,33 +1,29 @@
 package me.elb1to.watermc.hub.tasks;
 
-import me.elb1to.watermc.hub.user.HubPlayer;
+import me.elb1to.watermc.hub.Hub;
 import org.bukkit.Bukkit;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Elb1to
  * Project: Hub [WaterMC]
  * Date: 1/31/2021 @ 2:36 PM
  */
-@SuppressWarnings("InfiniteLoopStatement")
 public class PlayerDataWorkerRunnable implements Runnable {
 
 	@Override
 	public void run() {
-		ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-		executor.scheduleAtFixedRate(() -> {
-			while (true) {
-				Bukkit.getOnlinePlayers().forEach(player -> HubPlayer.getAllData().forEach(playerData -> {
-					if (!playerData.isDataLoaded()) {
-						HubPlayer hubPlayer = HubPlayer.getByUuid(player.getUniqueId());
-						hubPlayer.loadData(playerData);
-						playerData.setDataLoaded(true);
-					}
-				}));
+		while (true) {
+			try {
+				Thread.sleep(1000L);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		}, 0, 500, TimeUnit.MILLISECONDS);
+			Bukkit.getOnlinePlayers().forEach(player -> Hub.getInstance().getHubPlayerManager().getAllData().forEach(hubPlayer -> {
+				if (!hubPlayer.isDataLoaded()) {
+					Hub.getInstance().getHubPlayerManager().loadData(hubPlayer);
+					hubPlayer.setDataLoaded(true);
+				}
+			}));
+		}
 	}
 }
