84f5d1d29e4f8a484719aa4ae6ed4d36e0c4d41d
Victor Abrego <elbitomc@gmail.com>
2021-02-08 19:02:44 -0500

    Create NewHubPlayer.java

---
+++ src/main/java/me/elb1to/watermc/hub/user/NewHubPlayer.java
@@ -0,0 +1,30 @@
+package me.elb1to.watermc.hub.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.*;
+
+/**
+ * Created by Elb1to
+ * Project: Hub
+ * Date: 2/8/2021 @ 1:14 PM
+ */
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class NewHubPlayer {
+
+	private final UUID uniqueId;
+	private PlayerState playerState = PlayerState.LOBBY;
+
+	private String currentArmor;
+	private String currentParticle;
+
+	private boolean hidingParticles = false;
+	private boolean hidingPlayers = false;
+	private boolean flyMode = false;
+
+	private boolean dataLoaded;
+}
